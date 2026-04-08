95e891fdd6fd5e1f285e874a171d77509410e494
Angelina Guizar <angelina06jg@gmail.com>
2026-04-08 16:49:02 -0500

    Added a 3rd sphere variable and calculated volume

---
+++ volume_of_sphere.py
@@ -0,0 +1,9 @@
+def calculate_volume_of_sphere(radius):
+    volume = (4/3) * 3.14 * (radius**3)
+    return volume
+
+sphere1 = 30
+print(f"Volume of sphere 1 = {calculate_volume_of_sphere(sphere1)}")
+
+sphere2 = 40
+print(f"Volume of sphere 2 = {calculate_volume_of_sphere(sphere2)}")
